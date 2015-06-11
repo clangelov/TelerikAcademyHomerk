b37a17bc26c4911522491d27d920d6d44bd36276
Chavdar Angelov <c.l.angelov@gmail.com>
2015-06-11 21:38:46 +0300

    Regular-Expressions-Homework

---
+++ 06_JavaScript-Fundamentals/Regular-Expressions-Homework/01.FormatWithPlaceholders/FormatWithPlaceholders.js
@@ -0,0 +1,24 @@
+﻿/* Problem 1. Format with placeholders
+
+Write a function that formats a string. The function should have placeholders, as shown in the example
+Add the function to the String.prototype
+*/
+
+String.prototype.format = function (options) {
+
+    var regex,
+        input = this;
+
+    for (var key in options) {
+        regex = new RegExp('#{' + key + '}', 'g');
+        input = input.replace(regex, options[key]);
+    }
+
+    return input;
+}
+
+console.log('Original text is: Hello, there! Are you #{name}? and Object is: { name: \'John\' }')
+console.log('Changed text is: ' + 'Hello, there! Are you #{name}?'.format({ name: 'John' }));
+
+console.log('Original text is: My name is #{name} and I am #{age}-years-old).format(options) and object is: {name: \'Johnny\', age: 21} ');
+console.log('Changed text is: ' + 'My name is #{name} and I am #{age}-years-old)'.format({name: 'Johnny', age: 21}));
---
+++ 06_JavaScript-Fundamentals/Regular-Expressions-Homework/02.HTMLbinding/HTMLbinding.js
@@ -0,0 +1,34 @@
+﻿/* Problem 2. HTML binding
+
+Write a function that puts the value of an object into the content/attributes of HTML tags.
+Add the function to the String.prototype
+*/
+
+String.prototype.bind = function (string, object) {
+    var addContent,
+        addHref,
+        addClass,
+        result = this;
+
+    for (var key in object) {
+        addContent = new RegExp('(<.*data-bind-content="' + key + '".*>)(.*)(<.*>)', 'g'),
+        addHref = new RegExp('(<.*data-bind-href="' + key + '")', 'g'),
+        addClass = new RegExp('(data-bind-class="' + key + '")', 'g');
+
+        result = result.replace(addContent, function (none, opening, content, closing) {
+            content = object[key];
+            return opening + content + closing;
+        })
+            .replace(addHref, function (none, content) {
+                return content + ' href="' + object[key] + '"';
+            })
+            .replace(addClass, 'data-bind-class="' + object[key] + '"');
+    }
+    return result;
+};
+
+console.log('Original text is: <div data-bind-content="name"></div> and Object is: { name: \'Steven\' }')
+console.log('Changed text is: ' + '<div data-bind-content="name"></div>'.bind('', { name: 'Steven' }));
+
+console.log('Original text is: <a data-bind-content="name" data-bind-href="link" data-bind-class="name"></div> and Object is { name: \'Elena\', link: \'http://telerikacademy.com\' } ');
+console.log('Changed text is: ' + '<a data-bind-content="name" data-bind-href="link" data-bind-class="name"></div>'.bind('', { name: 'Elena', link: 'http://telerikacademy.com' }));
