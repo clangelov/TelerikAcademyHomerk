eaa0d86d5f250c13e0724528c304aa9257064012
Chavdar Angelov <c.l.angelov@gmail.com>
2015-07-11 10:16:36 +0300

    VariablesDataExpressions-Homework

---
+++ 08_High-QualityCode/VariablesDataExpressions-Homework/Task01.Figure/Figure.cs
@@ -0,0 +1,58 @@
+﻿/* Task 1. Class Size in C#
+ * StyleCop settings: disabled: Documentation Rules
+*/
+namespace Task01.Figure
+{
+    using System;
+
+    public class Figure
+    {
+        private double width;
+        private double height;
+
+        public Figure(double width, double heigth)
+        {
+            this.Width = width;
+            this.Height = heigth;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                this.height = value;
+            }
+        }
+
+        public static Figure GetRotatedFigure(Figure figure, double angleOfTheFigureToBeRotaed)
+        {
+            double newRotatedWidth = (Math.Abs(Math.Cos(angleOfTheFigureToBeRotaed)) * figure.width) +
+                    (Math.Abs(Math.Sin(angleOfTheFigureToBeRotaed)) * figure.height);
+
+            double newRotaredHeigth = (Math.Abs(Math.Sin(angleOfTheFigureToBeRotaed)) * figure.width) +
+                    (Math.Abs(Math.Cos(angleOfTheFigureToBeRotaed)) * figure.height);
+
+            Figure rotatedFigure = new Figure(newRotatedWidth, newRotaredHeigth);
+
+            return rotatedFigure;
+        }
+    }
+}
---
+++ 08_High-QualityCode/VariablesDataExpressions-Homework/Task02.MethodPrintStatistics/Statistics.cs
@@ -0,0 +1,41 @@
+﻿/* Task 2. Method PrintStatistics in C#
+ * StyleCop settings: disabled: Documentation Rules
+*/
+namespace Task02.MethodPrintStatistics
+{
+    using System;
+    using System.Linq;
+
+    internal class Statistics
+    {
+        public void PrintStatisticsToConsole(double[] numbers)
+        {
+            double maxNumber = this.FindMaxNumber(numbers);
+            Console.WriteLine(maxNumber);
+
+            double minNumber = this.FindMinNumber(numbers);
+            Console.WriteLine(minNumber);
+
+            double averageNumber = this.FindAverageNumber(numbers);
+            Console.WriteLine(averageNumber);
+        }
+
+        private double FindMaxNumber(double[] array)
+        {
+            double max = array.Max();
+            return max;
+        }
+
+        private double FindMinNumber(double[] array)
+        {
+            double min = array.Min();
+            return min;
+        }
+
+        private double FindAverageNumber(double[] array)
+        {
+            double average = array.Average();
+            return average;
+        }
+    }   
+}
