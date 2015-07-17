cfe46199e45e5a3a0168c70144c31fcfb391158d
Chavdar Angelov <c.l.angelov@gmail.com>
2015-07-17 15:05:44 +0300

    Classes-Homework

---
+++ 08_High-QualityCode/Classes-Homework/Task03.CohesionAndCoupling/FileUtils.cs
@@ -0,0 +1,43 @@
+﻿namespace Task03.CohesionAndCoupling
+{
+    using System;
+
+    using Task02.GoodEncapsulation;
+
+    internal static class FileUtils
+    {
+        internal static string GetFileExtension(string fileName)
+        {
+            Validator.CheckIfStringIsNullOrEmpty(fileName, "You can not pass null or empty parameter");
+
+            int indexOfLastDot = FindLastIndexOfDot(fileName);
+            if (indexOfLastDot == -1)
+            {
+                //Consider using throw new ArgumentException instead of returning string.Empty may not be a very good idea 
+                return string.Empty;
+            }
+
+            string extension = fileName.Substring(indexOfLastDot + 1);
+            return extension;
+        }
+
+        internal static string GetFileNameWithoutExtension(string fileName)
+        {
+            Validator.CheckIfStringIsNullOrEmpty(fileName, "You can not pass null or empty parameter");
+
+            int indexOfLastDot = FindLastIndexOfDot(fileName);
+            if (indexOfLastDot == -1)
+            {
+                return fileName;
+            }
+
+            string nameWithoutExtension = fileName.Substring(0, indexOfLastDot);
+            return nameWithoutExtension;
+        }
+
+        private static int FindLastIndexOfDot(string input)
+        {
+            return input.LastIndexOf(".");
+        }
+    }
+}
