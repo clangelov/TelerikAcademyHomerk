be1ef6f970d298d9342bc8861787e6e5b4848717
Chavdar Angelov <c.l.angelov@gmail.com>
2015-08-01 14:40:54 +0300

    DOM-Operations-Homework

---
+++ 09_JavaScript-UI_DOM/DOM-Operations-Homework/tasks/task-1.js
@@ -0,0 +1,85 @@
+/* globals $ */
+/*
+
+ Create a function that takes an id or DOM element and an array of contents
+
+ * if an id is provided, select the element
+ * Add divs to the element
+ * Each div's content must be one of the items from the contents array
+ * The function must remove all previous content from the DOM element provided
+ * Throws if:
+ * The provided first parameter is neither string or existing DOM element
+ * The provided id does not select anything (there is no element that has such an id)
+ * Any of the function params is missing
+ * Any of the function params is not as described
+ * Any of the contents is neither `string` or `number`
+ * In that case, the content of the element **must not be** changed
+ */
+
+module.exports = function () {
+
+    return function (element, contents) {
+
+        var content,
+            divEl,
+            nodeEl,
+            docFragment,
+            divElClone,
+            i,
+            length;
+
+        if (typeof element === undefined || contents === undefined) {
+            throw new {message: 'You need to pass an valid parameter'};
+        }
+
+        if (typeof element !== 'string' && !element.nodeType === 1) {
+            console.log('element error');
+            throw new {message: 'Parameter must be a string or node element'};
+        }
+
+        if (!Array.isArray(contents)) {
+            throw new {message: 'Contents must be an Array'};
+        }
+
+        contents.forEach(function (contentElement) {
+            if (typeof contentElement !== 'string' && typeof contentElement !== 'number') {
+                throw new {message: 'Content must be a string or number'};
+            }
+        });
+
+        if (typeof element === 'string') {
+
+            nodeEl = document.getElementById(element);
+
+            if (nodeEl === null) {
+                throw new {message: 'You passed a non existing ID'};
+            }
+
+            nodeEl.innerHTML = '';
+
+            divEl = document.createElement('div');
+            docFragment = document.createDocumentFragment();
+
+            for (i = 0, length = contents.length; i < length; i += 1) {
+
+                divElClone = divEl.cloneNode(true);
+                divElClone.innerHTML = contents[i];
+                docFragment.appendChild(divElClone);
+            }
+
+            nodeEl.appendChild(docFragment);
+
+        } else {
+
+            element.innerHTML = '';
+
+            content = '';
+
+            for (i = 0, length = contents.length; i < length; i += 1) {
+                content += '<div>' + contents[i] + '</div>';
+            }
+
+            element.innerHTML += content;
+        }
+    };
+};
