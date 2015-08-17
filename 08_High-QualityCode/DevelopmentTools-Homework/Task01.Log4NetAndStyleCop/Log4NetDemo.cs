6736e24e993f3de14eea697f4e12ea110e2ab972
Chavdar Angelov <c.l.angelov@gmail.com>
2015-08-17 15:53:22 +0300

    DevelopmentTools-Homework

---
+++ 08_High-QualityCode/DevelopmentTools-Homework/Task01.Log4NetAndStyleCop/Log4NetDemo.cs
@@ -0,0 +1,37 @@
+﻿// StyleCop settings:
+// Disabled: Documentation Rules
+// Violation Count: 0 
+namespace Task01.Log4NetAndStyleCop
+{
+    using System;
+    using log4net;
+    using log4net.Config;
+
+    public class Log4NetDemo
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Log4NetDemo));
+
+        public static void Main()
+        {
+            // This method initializes the log4net system to use a simple Console appender
+            BasicConfigurator.Configure();
+
+            Logger.Info("Start logging for this demo");
+
+            try
+            {
+                Logger.Warn("Performing a difficult operation");
+                int number = int.Parse("Useless string");    
+            }
+            catch (FormatException ex)
+            {
+                Logger.Error("You have an FormatException");
+                Console.WriteLine("An " + ex.GetType() + " occured when trying to parse a string");
+            }
+            finally
+            {
+                Logger.Fatal("This program could not be of any use");
+            }
+        }
+    }
+}
---
+++ 08_High-QualityCode/DevelopmentTools-Homework/Task02.T4Template/TextTemplateDemo.cs
@@ -0,0 +1,16 @@
+﻿namespace Task02
+{
+	using System;
+
+	public class SayHelloToPeter
+	{
+		static void Main()
+		{
+						Console.WriteLine("Hello, Peter!");
+						Console.WriteLine("Bonjour, Peter!");
+						Console.WriteLine("Aloha, Peter!");
+						Console.WriteLine("Kon-nichiwa, Peter!");
+						Console.WriteLine("Ola, Peter!");
+					}
+	}
+}
