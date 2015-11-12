51268a9dd837250009d451ffd31991e15b617ea4
Chavdar Angelov <c.l.angelov@gmail.com>
2015-11-12 23:08:11 +0200

    WCF-Homework

---
+++ 13_WebServices/WCF-Homework/Task05.StringCountConsoleClient/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace Task05.StringCountConsoleClient
+{
+    using System;
+    using StringOperation;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var client = new StringCounterClient();
+
+            string lookingFor = "day";
+            string text = "monday, tuesday, wednesday... firday";
+            int count = client.FindWordOccurance(text, lookingFor);
+            
+            Console.WriteLine("The word {0} contains {1} times.", lookingFor, count);
+        }
+    }
+}
